2a8c9bf457f29f908ad76dd9064eb384ec9d78c7
Filipe Santos <43495087+FCardeira@users.noreply.github.com>
2024-08-16 23:28:08 +0100

    Add OpenSeadragonAnnotator type to mountExtension function

--- annotorious-selector-pack/src/selectorPack.ts
+++ annotorious-selector-pack/src/selectorPack.ts
@@ -1,9 +1,10 @@
 import { ShapeType, type ImageAnnotator } from '@annotorious/annotorious';
+import { type OpenSeadragonAnnotator } from '@annotorious/openseadragon';
 import { EllipseEditor, RubberbandEllipse } from './ellipse';
 import type { SvelteComponent } from 'svelte';
 
 export const mountExtension = (
-  anno: ImageAnnotator
+  anno: ImageAnnotator | OpenSeadragonAnnotator
 ) => {
   anno.registerDrawingTool('ellipse', RubberbandEllipse as typeof SvelteComponent);
   anno.registerShapeEditor(ShapeType.ELLIPSE, EllipseEditor as typeof SvelteComponent);
